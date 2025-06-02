88a07c26ac3edc42e2176ff039061553b3e45e00
Suprymas <macysjustas1@gmail.com>
2025-06-02 13:59:36 +0300

    final

--- src/main/java/com/mano/codecademy/dinerApi/controller/RestaurantController.java
+++ src/main/java/com/mano/codecademy/dinerApi/controller/RestaurantController.java
@@ -1,34 +1,37 @@
 package com.mano.codecademy.dinerApi.controller;
 
+import com.mano.codecademy.dinerApi.DiningReview.DiningReview;
+import com.mano.codecademy.dinerApi.DiningReview.ReviewStatus;
 import com.mano.codecademy.dinerApi.User.DinerUser;
+import com.mano.codecademy.dinerApi.repositories.DiningReviewRepository;
 import com.mano.codecademy.dinerApi.repositories.RestaurantRepository;
 import com.mano.codecademy.dinerApi.repositories.UserRepository;
 import com.mano.codecademy.dinerApi.restaurant.Restaurant;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.web.bind.MissingRequestValueException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class RestaurantController {
 
     private final RestaurantRepository restaurantRepository;
     private final UserRepository userRepository;
-    public RestaurantController(RestaurantRepository restaurantRepository, UserRepository userRepository){
+    private final DiningReviewRepository diningReviewRepository;
+    public RestaurantController(RestaurantRepository restaurantRepository,
+                                UserRepository userRepository,
+                                DiningReviewRepository diningReviewRepository){
+
         this.restaurantRepository = restaurantRepository;
         this.userRepository = userRepository;
+        this.diningReviewRepository = diningReviewRepository;
     }
-
 
     @PostMapping("/createRestaurant")
     public Restaurant createRestaurant(@RequestBody Restaurant restaurant){ // Creating restaurant endpoint
-        if(restaurant.getName() == null || restaurant.getZipCode() == null){ // Making sure name and zip code are provided
+        if(restaurant.getName() == null || restaurant.getZipCode() == null){ // Making sure name and
+                                                                             // zip code are provided
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Name and ZipCode are required");
         }
         return this.restaurantRepository.save(restaurant);
@@ -48,6 +51,59 @@ public class RestaurantController {
         user.setIsInterestedInPeanuts(user.getIsInterestedInPeanuts() != null && user.getIsInterestedInPeanuts());
         return this.userRepository.save(user);
     }
+
+    @PostMapping("/updateUser")
+    public DinerUser updateUser(@RequestBody DinerUser user){
+        if(user.getName() == null){ //check if name is provided
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Name is required");
+        }
+        Optional<DinerUser> optionalDinerUser = this.userRepository.findByName(user.getName()); // check if userExists
+        if(optionalDinerUser.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User was not found");
+        }
+        DinerUser existingUser = optionalDinerUser.get();// get the user
+        if(user.getCity() != null) existingUser.setCity(user.getCity());
+        if(user.getZipCode() != null) existingUser.setZipCode(user.getZipCode());
+        if(user.getIsInterestedInEggs() != null) existingUser.setIsInterestedInEggs(user.getIsInterestedInEggs());
+
+        return this.userRepository.save(existingUser);
+    }
+
+    @GetMapping("/user")
+    public DinerUser getUser(@RequestParam String name){
+        Optional<DinerUser> optionalDinerUser = this.userRepository.findByName(name); // check if userExists
+        if(optionalDinerUser.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User was not found");
+        }
+        return optionalDinerUser.get();
+    }
+
+    @PostMapping("/submitReview")
+    public DiningReview submitDiningReview(@RequestBody DiningReview diningReview){
+        if (diningReview.getName()         == null ||
+            diningReview.getRestaurantId() == null ||
+            diningReview.getEggScore()     == null ||
+            diningReview.getDairyScore()   == null ||
+            diningReview.getPeanutScore()  == null ){
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing required data");
+        }
+
+        Optional<DinerUser> optionalDinerUser = this.userRepository.findByName(diningReview.getName());
+        if (optionalDinerUser.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User was not found");
+        }
+
+        return this.diningReviewRepository.save(diningReview);
+    }
+
+    @GetMapping("/pendingReviews")
+    public List<DiningReview> getPendingDiningReviews(){
+        return this.diningReviewRepository.findByStatus(ReviewStatus.PENDING);
+    }
+
+
+
 
 
 }
--- src/main/java/com/mano/codecademy/dinerApi/restaurant/Restaurant.java
+++ src/main/java/com/mano/codecademy/dinerApi/restaurant/Restaurant.java
@@ -8,14 +8,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
-    @Setter
-    @Getter
     private String zipCode;
     private Float peanutScore;
     private Float eggScore;
